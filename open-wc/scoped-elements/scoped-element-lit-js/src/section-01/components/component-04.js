e85e55d7523437758db166293c8b22bbbca83b63
Andres Giraldo <andresganc@gmail.com>
2023-04-01 12:30:39 -0500

    Mod: Open WC Scoped Elements Lit

--- open-wc/scoped-elements/scoped-element-lit-js/src/section-01/components/component-04.js
+++ open-wc/scoped-elements/scoped-element-lit-js/src/section-01/components/component-04.js
@@ -2,3 +2,5 @@
 import { Component04 } from './Component04.js'
 
 customElements.define('component-04', Component04)
+
+export { Component04 }
--- open-wc/scoped-elements/scoped-element-lit-js/src/section-02/components/component-03.js
+++ open-wc/scoped-elements/scoped-element-lit-js/src/section-02/components/component-03.js
@@ -2,3 +2,5 @@
 import { Component03 } from './Component03.js'
 
 customElements.define('component-03', Component03)
+
+export { Component03 }
